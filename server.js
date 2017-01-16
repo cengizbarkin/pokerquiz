8e826f9f229239191491f3b4358ba9a032baa46d
Cengiz Barkın Öztürk <cengiz@Hackbook-Pro.local>
2017-01-16 12:38:16 +0300

    Added Socket.io to the server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server);
 
 
+//Connecting User to the socketIO
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+});
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
