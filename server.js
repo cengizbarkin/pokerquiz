c9df9f449b1a2c3cc3ed56d5ade132df1c5ea689
Cengiz Barkın Öztürk <cengiz@Hackbook-Pro.local>
2017-01-16 17:03:10 +0300

    Continue to socket test

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ const server = http.createServer(app);
 
 const io = socketIO(server);
 
+//Different namespaces for user to connect games
+const silver = io.of('/silver');
+const golden = io.of('/golden');
+const diamond = io.of('/diamond');
+
+
+silver.on('connection', (socket) => {
+  console.log('Silver a birisi bağlandı');
+});
+
+
+golden.on('connection', (socket) => {
+  console.log('Golden a birisi bağlandı');
+});
+
+
+diamond.on('connection', (socket) => {
+  console.log('diamond a birisi bağlandı');
+});
+
+
 
 //Connecting User to the socketIO
 io.on('connection', (socket) => {
   console.log('New User Connected');
 
   socket.broadcast.emit('welcome');
-
+  socket.emit('message', 'Server', 'Hos Geldiniz');
 
 
   socket.on('deneme', (data) => {
