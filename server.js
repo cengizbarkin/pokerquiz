ec4a8e5bfef799ab0708c2d38ac9c2ea2f051102
Cengiz Barkın Öztürk <cengiz@Hackbook-Pro.local>
2017-01-16 12:48:43 +0300

    Added deneme socket

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ const io = socketIO(server);
 //Connecting User to the socketIO
 io.on('connection', (socket) => {
   console.log('New User Connected');
+
+  socket.on('deneme', (data) => {
+    console.log('Deneme cagirildi');
+  })
+
 });
 
 server.listen(port, () => {
